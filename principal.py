35849497ea84a2fa8891212f39341996603a65bc
Armando Soares Sousa <armando.sousa@gmail.com>
2023-07-08 12:05:22 +0000

    Modulo para carregar dados do banco em dataframes.

--- principal.py
+++ principal.py
@@ -7,3 +7,11 @@ import extracao
 
 # extrai informacoes de commits e arquivos modificados em cada commmit
 #extracao.extrai_informacoes_repositorio(my_repositorio='https://github.com/armandossrecife/promocity.git')
+
+import carrega_dataframes
+
+df_commits_from_db, df_files_from_db, df_files_commits_from_db = carrega_dataframes.load_dataframes(database_name=carrega_dataframes.DATA_BASE)
+
+print(df_commits_from_db)
+print(df_files_from_db)
+print(df_files_commits_from_db)

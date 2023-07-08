6c8b9e12ff25e4ba2c76df97cae5984e85d061ed
Armando Soares Sousa <armando.sousa@gmail.com>
2023-07-08 16:24:43 +0000

    Atualizado o modulo de analise de metricas com AMLOC, CC e scatter plots

--- analisa_metricas.py
+++ analisa_metricas.py
@@ -2,6 +2,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+# AMLOC de todos os arquivos
+# retorna o df_amloc, df_boxplot_amloc e group_files_modified_lines
 def get_accumulated_modified_locs(df_files_from_db):
   # Lista arquivos ordenados por nome com suas linhas modificadas
   df_files_from_db[['name','modified_lines']].sort_values(by=['name'], ascending=True)
@@ -29,24 +31,28 @@ def gera_boxplot_accumulated_modified_locs(df_accumulated_modified_locs_boxplot)
   sns.boxplot(x='File', y='modified_lines', data=df_accumulated_modified_locs_boxplot)
   plt.savefig('accumulated_modified_locs_boxplot.png')
 
+# Remove os arquivos que nao foram modificados
+# retorna o df_boxplot_amloc 
 def get_accumulated_modified_locs_boxplot_valid(df_boxplot_em):
   # Remove os arquivos que não foram modificados ao longo do tempo (O linhas modificadas)
   df_boxplot_em = df_boxplot_em.drop(df_boxplot_em[df_boxplot_em.modified_lines == 0].index)
-
   plt.figure(figsize=(6,4))
   # Constroi o Boxsplot sem os outliers
   sns.boxplot(x='File', y='modified_lines', data=df_boxplot_em,  showfliers=False)
   plt.savefig('accumulated_modified_locs_boxplot_valid.png')
   return df_boxplot_em
 
+# Cria um df_accumulated_modified_java_files contendo apenas arquivos .java
+# df_accumulated_modified_java_files
 def get_df_accumulated_modified_java_files(df_accumulated_modified_locs):
   # Cria um df sem os arquivos de Teste
   df_locm_no_test = df_accumulated_modified_locs[(df_accumulated_modified_locs["name"].str.contains('Test') == False)]
-
   # Cria um df contendo apenas os arquivos .java de implementacao
   df_locm_java_impl = df_locm_no_test[df_locm_no_test['name'].str.contains('.java', regex=False)]
   return df_locm_java_impl
 
+# mostra o boxplot accumulated_modified_java_files
+# retorna df_boxplot_em_java_impl contendo apenas arquivos .java
 def show_boxplot_accumulated_modified_java_files(df_locm_java_impl):
   df_boxplot_em_java_impl = df_locm_java_impl[['modified_lines', 'File']]
   plt.figure(figsize=(6,4))
@@ -54,6 +60,7 @@ def show_boxplot_accumulated_modified_java_files(df_locm_java_impl):
   plt.savefig('accumulated_modified_locs_boxplot_java_files.png')
   return df_boxplot_em_java_impl
 
+# Calcula os quartiles de todos os arquivos referente a AMLOC
 def get_quartiles_offiles_modified_lines(group_files_modified_lines, df_boxplot_em):
   list_of_files_modified_lines = group_files_modified_lines.to_dict()
   print(f'{ len(list_of_files_modified_lines) }, {list_of_files_modified_lines}')
@@ -65,6 +72,8 @@ def get_quartiles_offiles_modified_lines(group_files_modified_lines, df_boxplot_
   print(f'Quartis do Total de Linhas Modificadas: Q1: {em_q1}, Q2: {em_q2}, Q3: {em_q3}, Q4: {em_q4}')
   return em_q1, em_q2, em_q3, em_q4
 
+# Seleciona apenas os AMLOC validos (arquivos que foram modificados pelo menos uma vez)
+# retorna o df_boxplot_em_java_impl
 def get_accumulated_modified_java_files_valid(df_boxplot_em_java_impl):
   # Remove os arquivos que não foram modificados ao longo do tempo (O linhas modificadas)
   df_boxplot_em_java_impl = df_boxplot_em_java_impl.drop(df_boxplot_em_java_impl[df_boxplot_em_java_impl.modified_lines == 0].index)
@@ -75,6 +84,7 @@ def get_accumulated_modified_java_files_valid(df_boxplot_em_java_impl):
   plt.savefig('accumulated_modified_locs_boxplot_java_files_valid.png')
   return df_boxplot_em_java_impl
 
+# Calcula os quartiles dos AMLOC .java validos
 def get_quartiles_offiles_modified_lines_java_files_valid(df_boxplot_em_java_impl):
   # Mostra os quatis 
   em_q1_java_impl = np.percentile(df_boxplot_em_java_impl.modified_lines, [25])
@@ -84,7 +94,62 @@ def get_quartiles_offiles_modified_lines_java_files_valid(df_boxplot_em_java_imp
   print(f'Quartis do Total de Linhas Modificadas: Q1: {em_q1_java_impl}, Q2: {em_q2_java_impl}, Q3: {em_q3_java_impl}, Q4: {em_q4_java_impl}')
   return em_q1_java_impl, em_q2_java_impl, em_q3_java_impl, em_q4_java_impl
 
+# Cria um df de complexidade ciclomatica de todos os arquivos
+# retorna df_cc_temp
 def get_complexidade_ciclomatica(df_files_commits_from_db):
+  # Mostra as Complexidades Ciclomáticas dos arquivos ordenada crescent pelo tempo
+  df_cc = df_files_commits_from_db[['file_filename', 'file_complexity', 'author_date']].sort_values(by=['file_filename', 'author_date'], ascending=True)
+  # Remove files that has not cc
+  # cc all files
+  df_cc_temp = df_cc.copy()
+  df_cc_temp = df_cc_temp[df_cc_temp.file_complexity.notnull()]
+  return df_cc_temp
+
+##### Todos os arquivos #####
+# gera um boxplot da complexidade ciclomatica de todos os arquivos
+def get_boxplot_complexidade_ciclomatica(df_cc_temp):
+  df_cc_temp['File'] = 'File'
+  df_boxplot_cc_temp = df_cc_temp[['file_complexity', 'File']]
+  plt.figure(figsize=(6,4))
+  sns.boxplot(x='File', y='file_complexity', data=df_boxplot_cc_temp)
+  plt.savefig('boxplot_complexidade_ciclomatica.png')
+  return df_cc_temp, df_boxplot_cc_temp
+
+# Calcula os quartiles das complexidades ciclomaticas
+# retorna os quartiles
+def get_quartiles_complexidade_ciclomatica(df_boxplot_cc_temp):
+  # Mostra os quatis 
+  em_q1_cc_temp = np.percentile(df_boxplot_cc_temp.file_complexity, [25])
+  em_q2_cc_temp = np.percentile(df_boxplot_cc_temp.file_complexity, [50])
+  em_q3_cc_temp = np.percentile(df_boxplot_cc_temp.file_complexity, [75])
+  em_q4_cc_temp = np.percentile(df_boxplot_cc_temp.file_complexity, [100])
+
+  print(f'Quartis do Total de Linhas Modificadas: Q1: {em_q1_cc_temp}, Q2: {em_q2_cc_temp}, Q3: {em_q3_cc_temp}, Q4: {em_q4_cc_temp}')
+  return em_q1_cc_temp, em_q2_cc_temp, em_q3_cc_temp, em_q4_cc_temp
+
+### Apenas os arquivos .java ###
+# gera um boxplot de complexidade ciclomatica apenas dos arquivos .java
+def get_boxplot_complexidade_ciclomatica_only_java(df_cc_temp):
+  # Cria um df sem os arquivos de Teste
+  df_cc_temp_no_test = df_cc_temp[(df_cc_temp["file_filename"].str.contains('Test') == False)]
+  # Cria um df contendo apenas os arquivos .java de implementacao
+  df_cc_temp_java_impl = df_cc_temp_no_test[df_cc_temp_no_test['file_filename'].str.contains('.java', regex=False)]
+  df_cc_temp_boxplot_em_java_impl = df_cc_temp_java_impl[['file_complexity', 'File']]
+
+  plt.figure(figsize=(6,4))
+  sns.boxplot(x='File', y='file_complexity', data=df_cc_temp_boxplot_em_java_impl)
+  plt.savefig('boxplot_complexidade_ciclomatica_only_java.png')
+  return df_cc_temp_java_impl, df_cc_temp_boxplot_em_java_impl
+
+# calcula os quatiles das complexidade ciclomaticas apenas dos arquivos .java
+# retorna os quartiles
+def get_quartiles_complexidade_ciclomatica_java_impl(df_cc_temp_boxplot_em_java_impl):
+  em_q1_cc_temp_java_impl = np.percentile(df_cc_temp_boxplot_em_java_impl.file_complexity, [25])
+  em_q2_cc_temp_java_impl = np.percentile(df_cc_temp_boxplot_em_java_impl.file_complexity, [50])
+  em_q3_cc_temp_java_impl = np.percentile(df_cc_temp_boxplot_em_java_impl.file_complexity, [75])
+  em_q4_cc_temp_java_impl = np.percentile(df_cc_temp_boxplot_em_java_impl.file_complexity, [100])
+  print(f'Quartis do Total de Linhas Modificadas: Q1: {em_q1_cc_temp_java_impl}, Q2: {em_q2_cc_temp_java_impl}, Q3: {em_q3_cc_temp_java_impl}, Q4: {em_q4_cc_temp_java_impl}')
+  return em_q1_cc_temp_java_impl, em_q2_cc_temp_java_impl, em_q3_cc_temp_java_impl, em_q4_cc_temp_java_impl
   # Mostra as Complexidades Ciclomáticas dos arquivos ordenada crescent pelo tempo
   df_cc = df_files_commits_from_db[['file_filename', 'file_complexity', 'author_date']].sort_values(by=['file_filename', 'author_date'], ascending=True)
   df_cc.query("file_filename == 'UserController.java'")
--- principal.py
+++ principal.py
@@ -12,25 +12,31 @@ import carrega_dataframes
 
 df_commits_from_db, df_files_from_db, df_files_commits_from_db = carrega_dataframes.load_dataframes(database_name=carrega_dataframes.DATA_BASE)
 
-#print(df_commits_from_db)
-#print(df_files_from_db)
-#print(df_files_commits_from_db)
-
-# Faz alguns ajustes nos dataframes
+# Faz alguns ajustes nos dataframes para o calculo de AMLOC
 df_files_from_db['modified_lines'] = df_files_from_db.added_lines + df_files_from_db.deleted_lines
 df_files_commits_from_db['modified_lines'] = df_files_commits_from_db.file_added_lines + df_files_commits_from_db.file_deleted_lines
 
 import analisa_metricas
 
+print('Calcula o AMLOC')
 df_accumulated_modified_locs, df_accumulated_modified_locs_boxplot, group_files_modified_lines = analisa_metricas.get_accumulated_modified_locs(df_files_from_db)
+
+print('Gera o boxplot de AMLOC')
 df_accumulated_modified_locs_boxplot_validos = analisa_metricas.get_accumulated_modified_locs_boxplot_valid(df_accumulated_modified_locs_boxplot)
+
+print('Calcula os quartiles de AMLOC')
 em_q1, em_q2, em_q3, em_q4 = analisa_metricas.get_quartiles_offiles_modified_lines(group_files_modified_lines,df_accumulated_modified_locs_boxplot_validos)
 
+print('Calcula o AMLOC dos arquivos .java')
 df_locm_java_impl = analisa_metricas.get_df_accumulated_modified_java_files(df_accumulated_modified_locs)
+print('Gera o boxplot de AMLOC dos arquivos .java')
 df_boxplot_em_java_impl = analisa_metricas.show_boxplot_accumulated_modified_java_files(df_locm_java_impl)
 
+print('Calcula o AMLOC dos arquivos .java (Válidos - arquivos que modificaram pelo menos uma vez)')
 df_boxplot_em_java_impl_valid = analisa_metricas.get_accumulated_modified_java_files_valid(df_boxplot_em_java_impl)
+print('Calcula os quartiles referente ao AMLOC dos arquivos .java')
 em_q1_java_impl, em_q2_java_impl, em_q3_java_impl, em_q4_java_impl = analisa_metricas.get_quartiles_offiles_modified_lines_java_files_valid(df_boxplot_em_java_impl_valid)
 
+print('Calculo da Complexidade Ciclomatica')
 df_complexidade_ciclomatica = analisa_metricas.get_complexidade_ciclomatica(df_files_commits_from_db)
 print(df_complexidade_ciclomatica)

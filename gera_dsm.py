ed6bcd186d35146a128781c1687f965c87abc06b
Armando Soares Sousa <armando.sousa@gmail.com>
2023-08-17 16:46:47 +0000

    Testa forma simplificada de gerar DSM.

--- gera_dsm.py
+++ gera_dsm.py
@@ -1,6 +1,7 @@
 import os
 from plyj.parser import Parser
 import numpy as np
+import logging
 
 class JavaFileAnalyzer:
     def __init__(self, repo_folder, dsm_filename):
@@ -8,13 +9,39 @@ class JavaFileAnalyzer:
         self.java_files = []
         self.dependency_matrix = {}
         self.dsm_filename = dsm_filename
+        self.all_classes = []
         self.parser = Parser()
+
+        # Configure logging
+        self.log_file = 'JavaFileAnalyzer_error_log.txt'
+        logging.basicConfig(filename=self.log_file, level=logging.ERROR,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+
     
     def find_java_files(self):
         for root, dirs, files in os.walk(self.repo_folder):
             for file in files:
                 if file.endswith(".java"):
                     self.java_files.append(os.path.join(root, file))
+
+    def collect_class_names(self):
+        for java_file in self.java_files:
+            with open(java_file, "r") as file:
+                content = file.read()
+                try:
+                    tree = self.parser.parse_string(content)
+                    if tree is not None: 
+                        for type_declaration in tree.type_declarations:
+                            if type_declaration is not None:
+                                if hasattr(type_declaration, 'name') and type_declaration.name is not None:
+                                    self.all_classes.append(type_declaration)
+                                    print(str(type_declaration.name))
+                                    if type_declaration.extends is not None:
+                                        print(' -> extending ' + type_declaration.extends.name)
+                                    if len(type_declaration.implements) is not 0:
+                                        print(' -> implementing ' + ', '.join([type.name for type in type_declaration.implements]))
+                except Exception as e:
+                    logging.error(f"Error parsing {java_file}: {e}")
     
     def analyze_dependencies(self):
         for java_file in self.java_files:
@@ -29,7 +56,7 @@ class JavaFileAnalyzer:
                             type_name = type_declaration.name
                             self.dependency_matrix[type_name] = imports
                 except Exception as e:
-                    print(f"Error parsing {java_file}: {e}")
+                    logging.error(f"Error parsing {java_file}: {e}")
 
     def generate_dsm(self):
         all_types = set(self.dependency_matrix.keys())
